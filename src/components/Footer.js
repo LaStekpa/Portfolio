9cec5b7092ffb9200f8b6a0d005e005889b7951f
Benway Teh <teh.benway@gmail.com>
2024-10-02 00:17:13 +0200

    add Footer.js component

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <div className="containerimg">
+        <div className="row">
+          <div className="col-12">
+            <p className="d-flex justify-content-center">TEH BENWAY ©</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
